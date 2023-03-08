b5bc865fb86a34a4b2cc7b7876bb7572fc291708
Paul Ralnikov <st106332@student.spbu.ru>
2023-03-08 15:51:24 +0300

    Makefile changes(depences) + operators fixes

--- lab-07_matrix/include/matrix.h
+++ lab-07_matrix/include/matrix.h
@@ -7,6 +7,8 @@
 class Matrix {
 public:
   Matrix(size_t r, size_t c);
+  Matrix(const Matrix& m);
+  ~Matrix();
 
   size_t get_rows() const;
   size_t get_cols() const;
@@ -14,16 +16,19 @@ public:
   int get(size_t i, size_t j) const;
   void print(FILE *f) const;
 
-  friend Matrix operator+(const Matrix& m1, const Matrix& m2);
-  friend Matrix operator-(const Matrix& m1, const Matrix& m2);
-  friend Matrix operator*(const Matrix& m1, const Matrix& m2);
+  Matrix operator+(const Matrix& m) const;
+  Matrix operator-(const Matrix& m) const;
+  Matrix operator*(const Matrix& m) const;
 
   Matrix& operator+=(const Matrix& m);
   Matrix& operator-=(const Matrix& m);
   Matrix& operator*=(const Matrix& m);
 
-  bool operator==(Matrix& m) const;
-  bool operator!=(Matrix& m) const;
+  bool operator==(const Matrix& m) const;
+  bool operator!=(const Matrix& m) const;
+
+
+  Matrix& operator=(const Matrix& m);
 private:
   size_t _rows;
   size_t _cols;
--- lab-07_matrix/src/matrix.cpp
+++ lab-07_matrix/src/matrix.cpp
@@ -14,6 +14,43 @@ Matrix::Matrix(size_t r, size_t c) {
 	}
 }
 
+Matrix::Matrix(const Matrix& m) {
+	_rows = m.get_rows();
+	_cols = m.get_cols();
+	_data = new int*[_rows];
+	for (size_t i = 0; i < _rows; i++) {
+		_data[i] = new int[_cols];
+		for (size_t j = 0; j < _cols; j++) {
+			_data[i][j] = m.get(i, j);
+		}
+	}
+}
+
+Matrix::~Matrix() {
+	for (size_t i = 0; i < _rows; i++) {
+		delete[] _data[i];
+	} 
+	delete[] _data;
+}
+
+Matrix& Matrix::operator=(const Matrix& m) {
+	if (*this == m) return *this;
+	
+	for (size_t i = 0; i < _rows; i++) 
+		delete[] _data[i];
+	delete [] _data;
+
+	_rows = m.get_rows();
+	_cols = m.get_cols();
+	_data = new int*[_rows];	
+	for (size_t i = 0; i < _rows; i++) {
+		_data[i] = new int[_cols];
+		for (size_t j = 0; j < _cols; j++) 
+			_data[i][j] = m.get(i, j);
+	}
+	return *this;
+}
+
 size_t Matrix::get_rows() const {
 	return _rows;
 }
@@ -42,57 +79,59 @@ void Matrix::print(FILE* f) const {
 	for (size_t i = 0; i < _rows; i++) {
 		for (size_t j = 0; j < _cols; j++) {
 			fprintf(f, "%d", _data[i][j]);
-			fprintf(f, " ");
+			if (j != _cols - 1) fprintf(f, " ");
 		}
 		fprintf(f, "\n");
 	}
 }
 
-Matrix operator+ (const Matrix& m1, const Matrix& m2) {
-	if (m1._rows != m2._rows || m1._cols != m2._cols) 
-		throw new std::runtime_error("Sizes of summands are not equal");
-	size_t r = m1._rows;
-	size_t c = m1._cols;
-	Matrix m(r, c);
-	for (size_t i = 0; i < r; i++) {
-		for (size_t j = 0; j < c; j++) 
-			m.set(i, j, m1.get(i, j) + m2.get(i, j));
-	}
-	return m;
+Matrix Matrix::operator+(const Matrix& m) const {
+    if (_rows != m.get_rows() || _cols != m.get_cols()) {
+        throw std::runtime_error("Sizes are different");
+    }
+ 
+    Matrix answer(_rows, _cols);
+    for (size_t i = 0; i < _rows; i++) {
+        for (size_t j = 0; j < _cols; j++) {
+            answer.set(i, j, _data[i][j] + m.get(i, j));
+        }
+    }
+ 
+    return answer;
 }
-
-Matrix operator- (const Matrix& m1, const Matrix& m2) {
-	if (m1._rows != m2._rows || m1._cols != m2._cols) 
-		throw new std::runtime_error(
-			"Sizes of subtrahend are not equal to sizes of minuend");
-	size_t r = m1._rows;
-	size_t c = m1._cols;
-	Matrix m(r, c);
-	for (size_t i = 0; i < r; i++) {
-		for (size_t j = 0; j < c; j++) 
-			m.set(i, j, m1.get(i, j) - m2.get(i, j));
-	}
-	return m;
+ 
+Matrix Matrix::operator-(const Matrix& m) const {
+    if (_rows != m.get_rows() || _cols != m.get_cols()) {
+        throw std::runtime_error("Sizes are different");
+    }
+ 
+    Matrix answer(_rows, _cols);
+    for (size_t i = 0; i < _rows; i++) {
+        for (size_t j = 0; j < _cols; j++) {
+            answer.set(i, j, _data[i][j] - m.get(i, j));
+        }
+    }
+ 
+    return answer;
 }
-
-Matrix operator*(const Matrix& m1, const Matrix& m2) {
-	if (m1._cols != m2._rows) 	
-		throw new std::runtime_error(
-			"Incorrect sizes in multiply");
-	size_t r = m1._rows;
-	size_t c = m2._cols;
-	Matrix m(r, c);
-	int sum = 0;
-	for (size_t i = 0; i < r; i++) {
-		for (size_t j = 0; j < c; j++) {
-			sum = 0;
-			for (size_t k = 0; k < m1._cols; k++) {
-				sum += m1.get(i, k) * m2.get(k, j);
-			}
-			m.set(i, j, sum);
-		}
-	}
-	return m;
+ 
+Matrix Matrix::operator*(const Matrix& m) const {
+    if (_cols != m.get_rows()) {
+        throw std::runtime_error("Sizes do not match");
+    }
+ 
+    Matrix answer(_rows, m.get_cols());
+    for (size_t i = 0; i < answer.get_rows(); i++) {
+        for (size_t j = 0; j < answer.get_cols(); j++) {
+            int sum = 0;
+            for (size_t t = 0; t < _cols; ++t) {
+                sum += _data[i][t] * m.get(t, j);
+            }
+            answer.set(i, j, sum);
+        }
+    }
+ 
+    return answer;
 }
 
 Matrix& Matrix::operator+=(const Matrix& m) {
@@ -107,16 +146,26 @@ Matrix& Matrix::operator*=(const Matrix& m) {
 	return *this = *this * m;
 }
 
-bool Matrix::operator==(Matrix& m) const{
-	if (_rows != m.get_rows() || _cols != m.get_cols()) return false;
-	for (size_t i = 0; i < _rows; i++) {
-		for (size_t j = 0; j < _cols; j++) {
-			if (_data[i][j] != m.get(i, j)) return false;
-		}
-	}
-	return true;
+bool Matrix::operator==(const Matrix& m) const {
+    if (this == &m) {
+        return true;
+    }
+ 
+    if (_rows != m.get_rows() || _cols != m.get_cols()) {
+        return false;
+    }
+ 
+    for (size_t row = 0; row < _rows; ++row) {
+        for (size_t col = 0; col < _cols; ++col) {
+            if (_data[row][col] != m.get(row, col)) {
+                return false;
+            }
+        }
+    }
+ 
+    return true;
 }
 
-bool Matrix::operator !=(Matrix& m) const{
-	return !(*this == m);
+bool Matrix::operator!=(const Matrix& m) const {
+    return !(*this == m);
 }
