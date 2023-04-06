0c2854c71c5b8949b23511f0c8b5dfc4e2a8983b
Paul Ralnikov <st106332@student.spbu.ru>
2023-04-06 11:24:52 +0300

    Memory allocation in constructor fixed

--- lab-11_vector/include/my_vector_impl.h
+++ lab-11_vector/include/my_vector_impl.h
@@ -9,7 +9,9 @@ namespace containers {
 		size_ = n;
 		capacity_ = 1;
 		while (capacity_ < n) capacity_ *= 2;
-		array_ = new T[capacity_];
+		array_ = calloc(n, sizeof(T));
+		for (std::size_t i = 0; i < n; i++)
+			array_[i] = T();
 	}
 
 	template<typename T>
