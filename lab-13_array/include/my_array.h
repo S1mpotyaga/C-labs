05b13dfa08eb7a2de3d3208ddca3486e9679bc6a
Paul Ralnikov <st106332@student.spbu.ru>
2023-04-22 21:00:53 +0300

    Work without bool specification

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -2,21 +2,87 @@
 #define MY_ARRAY_H_
 
 #include <cstddef>
+#include <stdlib.h>
+#include <stdexcept>
 
 namespace lab_13 {
 
 template<typename T, std::size_t N>
 class my_array {
 public:
-    at(std::size_t index);
-    operator[](std::size_t index);
+    my_array();
+    
+    T& at(std::size_t index);
+    const T& at(std::size_t index) const;
+    T& operator[](std::size_t index);
+    const T& operator[](std::size_t index) const;
 
-    bool empty();
-    size();
 
-    fill(T val);
+    bool empty() const;
+    std::size_t size() const;
+
+    void fill(const T& val);
+
+    void operator= (const my_array<T, N>& from);
+private:
+    T array_[N];
 };
 
+template<typename T, std::size_t N>
+my_array<T, N>::my_array() {
+    for (size_t i = 0; i < N; i++) {
+        new (array_ + i) T();
+    }
+}
+
+template<typename T, std::size_t N>
+T& my_array<T, N>::at(std::size_t i) {
+    if (i >= N) throw std::out_of_range("Index out of range");
+    return array_[i];
+}
+
+template<typename T, std::size_t N>
+const T& my_array<T, N>::at(std::size_t i) const{
+    if (i >= N) throw std::out_of_range("Index out of range");
+    return array_[i];
+}
+
+template<typename T, std::size_t N>
+T& my_array<T, N>::operator[](std::size_t i) {
+    return array_[i];
+}
+
+template<typename T, std::size_t N>
+const T& my_array<T, N>::operator[](std::size_t i) const {
+    return array_[i];
+}
+
+template<typename T, std::size_t N>
+bool my_array<T, N>::empty() const{
+    return N == 0;
+}
+
+template<typename T, std::size_t N>
+size_t my_array<T, N>::size() const{
+    return N;
+}
+
+template<typename T, std::size_t N>
+void my_array<T, N>::fill(const T& val) {
+    for (std::size_t i = 0; i < N; i++) {
+        array_[i] = val;
+    }
+}
+
+template<typename T, std::size_t N>
+void my_array<T, N>::operator= (const my_array<T, N>& from) {
+    for (size_t i = 0; i < N; i++) {
+        array_[i] = from[i];
+    }
+}
+
 }  // namespace lab_13
+
+
 
 #endif  // MY_ARRAY_H_
