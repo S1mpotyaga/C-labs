38f494c0493a106b08cd1f91824056a0082952f1
S1mpotyaga <zfk.2024@gmail.com>
2025-02-06 18:45:06 +0500

    without Makefile

---
+++ lab-01_makefile/src/algorithm.c
@@ -0,0 +1,8 @@
+#include "algorithm.h"
+#include "io.h"
+
+int doComputation(int a, double b) {
+    int c = max(3, 4);
+    logDebug('d');
+    return c;
+}
---
+++ lab-01_makefile/src/io.c
@@ -0,0 +1,5 @@
+#include "io.h"
+
+void logDebug(char c) {
+	printf("%c", c);
+}
