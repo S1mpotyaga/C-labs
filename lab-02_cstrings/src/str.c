5c1df28e398cfddebd7899655379186861f09b75
S1mpotyaga <zfk.2024@gmail.com>
2025-02-09 19:23:28 +0500

    all function

--- lab-02_cstrings/src/str.c
+++ lab-02_cstrings/src/str.c
@@ -1,22 +1,37 @@
 #include "str.h"
 
 char *strcpy(char *dest, char *src){
-	char *result = dest;
-	for (; src != nullptr; src++){
-		*dest = *src;
-		dest++;
-	}
-	*dest = *src;
-	return result;
+	char *it_dest = dest;
+	char *it_src = src;
+	for (; *it_src != '\0'; it_src++, it_dest++)
+		*it_dest = *it_src;
+	return dest;
 }
 
 char *strcat(char *dest, char *src){
+	char *it_dest = dest;
+	while (*it_dest != '\0')
+		it_dest++;
+	it_dest = strcpy(it_dest, src);
+	return dest;
 }
 
 int strcmp(char *lhs, char *rhs){
-
+	char *it_lhs = lhs;
+	char *it_rhs = rhs;
+	for (; *it_lhs != '\0' && *it_rhs != '\0'; it_lhs++, it_rhs++){
+		int tmp = *it_lhs - *it_rhs;
+		if (tmp < 0)
+			return tmp;
+		else if (tmp > 0)
+			return tmp;
+	}
+	return 0;
 }
 
 size_t strlen(char *str){
-
+	char *it_str = str;
+	while (*it_str != '\0')
+		it_str++;
+	return (size_t)(it_str - str);
 }
