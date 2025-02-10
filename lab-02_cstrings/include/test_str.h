1fec3628aa51c6a815acab391e91ab73563b7a36
S1mpotyaga <zfk.2024@gmail.com>
2025-02-10 20:21:38 +0500

    all check

--- lab-02_cstrings/include/test_str.h
+++ lab-02_cstrings/include/test_str.h
@@ -2,4 +2,12 @@
 
 #define _TEST_STR_
 
+void test_strcpy();
+
+void test_strcat();
+
+void test_strcmp();
+
+void test_strlen();
+
 #endif
--- lab-02_cstrings/src/test_str.c
+++ lab-02_cstrings/src/test_str.c
@@ -1,1 +1,41 @@
 #include "test_str.h"
+#include <assert.h>
+#include <string.h>
+
+void test_strcpy(){
+	char a[] = "Hello", b[] = "aba", c[] = "";
+	char a_cpy[10] = "",  b_cpy[10] = "acd", c_cpy[10] = "acd";
+	assert(my_strcpy(a_cpy, a) == a);
+	assert(my_strcpy(b_cpy, b) == b);
+	assert(my_strcpy(c_cpy, c) == c);
+}
+
+void test_strcat(){
+	char a[] = "Hello", b[] = "", c[] = "me";
+	char a_tmp[] = "!", b_tmp[] = "Hello!", c_tmp[] = ":)";
+	char check1[] = "Hello!", check2[] = "me:)";
+	assert(strcmp(my_strcat(a, a_tmp), check1) == 0);
+	assert(strcmp(my_strcat(b, b_tmp), check1) == 0);
+	assert(strcmp(my_strcat(c, c_tmp), check2) == 0);
+}
+
+bool check(int a, int b){
+	if (a == 0 && b == 0)
+		return true;
+	return a * b * 1ll > 0;
+}
+
+void test_strcmp(){
+	char a[] = "aba", b[] = "ajfgkerngjea", c[] = "";
+	assert(check(my_strcmp(a, b), strcmp(a, b)));
+	assert(check(my_strcmp(a, c), strcmp(a, c)));
+	assert(check(my_strcmp(b, c), strcmp(b, c)));
+}
+
+void test_strlen(){
+	char a[] = "ajbsdfjkne", b[] = "", c[] = "\n";
+	assert(my_strlen(a) == strlen(a));
+	assert(my_strlen(b) == strlen(b));
+	assert(my_strlen(c) == strlen(c));
+}
+}
