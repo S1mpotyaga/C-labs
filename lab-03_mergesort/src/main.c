bc2f81f9a95d4b602c429b6013acbdae2c3ba5b2
S1mpotyaga <zfk.2024@gmail.com>
2025-07-12 19:17:28 +0500

    rename files

---
+++ lab-03_mergesort/src/main.c
@@ -0,0 +1,60 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../include/mergesort.h"
+
+int comporator_int(const void* a, const void* b){
+	return *(int*)a - *(int*)b;
+}
+
+int comporator_char(const void* a, const void* b){
+	return *(char*)a - *(char*)b;
+}
+
+int comporator_str(const void* a, const void* b){
+	return strcmp(*(char**)a, *(char**)b);
+}
+
+int main(int argc, char* argv[]){
+	size_t n = argc - 2;
+	if (strcmp(argv[1], "int") == 0){
+		int a[n];
+		for (size_t i = 0; i < n; ++i){
+			a[i] = atoi(argv[i + 2]);
+		}
+		if (mergesort(a, n, sizeof(a[0]), comporator_int) != 0){
+			printf("Error!");
+			return 0;
+		}
+		for (size_t i = 0; i < n; ++i){
+			printf("%d ", a[i]);
+		}
+	} else if (strcmp(argv[1], "char") == 0){
+		char a[n];
+		for (size_t i = 0; i < n; ++i){
+			a[i] = argv[i + 2][0];
+		}
+		if (mergesort(a, n, sizeof(a[0]), comporator_char) != 0){
+			printf("Error!");
+			return 0;
+		}
+		for (size_t i = 0; i < n; ++i){
+			printf("%c ", a[i]);
+		}
+	} else if (strcmp(argv[1], "str") == 0){
+		char* a[n];
+		for (size_t i = 0; i < n; ++i){
+			a[i] = argv[i + 2];
+		}
+		if (mergesort(a, n, sizeof(a[0]), comporator_str) != 0){
+			printf("Error!");
+			return 0;
+		}
+		for (size_t i = 0; i < n; ++i){
+			printf("%s ", a[i]);
+		}
+	}
+	printf("\n");
+}
---
+++ lab-03_mergesort/src/mergesort.c
@@ -0,0 +1,54 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mergesort(
+	void* array,
+	size_t elements, size_t element_size,
+	int (*comparator)(const void*, const void*)
+){
+	if (elements < 2){
+		return 0;
+	}
+	size_t leftElements = elements / 2;
+	size_t rightElements = elements - leftElements;
+	char* arrayCopy = (char*)array;
+	if (mergesort(array, leftElements, element_size, comparator) != 0){
+		return -1;
+	}
+	if (mergesort((char*)array + leftElements * element_size, rightElements, element_size, comparator) != 0){
+		return -1;
+	}
+	void* result = malloc(elements * element_size);
+	if (!result){
+		return -1;
+	}
+	size_t indLeft = 0, indRight = leftElements, indResult = 0;
+	while (indLeft < leftElements && indRight < elements){
+		void* leftCurrent = (char*)arrayCopy + indLeft * element_size;
+		void* rightCurrent = (char*)arrayCopy + indRight * element_size;
+		if (comparator(leftCurrent, rightCurrent) < 0){
+			memcpy((char*)result + indResult * element_size, leftCurrent, element_size);
+			indLeft++;
+		} else{
+			memcpy((char*)result + indResult * element_size, rightCurrent, element_size);
+			indRight++;
+		}
+		indResult++;
+	}
+	while (indLeft < leftElements){
+		void* leftCurrent = (char*)arrayCopy + indLeft * element_size;
+		memcpy((char*)result + indResult * element_size, leftCurrent, element_size);
+		indLeft++;
+		indResult++;
+	}
+	while (indRight < elements){
+		void* rightCurrent = (char*)arrayCopy + indRight * element_size;
+		memcpy((char*)result + indResult * element_size, rightCurrent, element_size);
+		indRight++;
+		indResult++;
+	}
+	memcpy(arrayCopy, result, elements * element_size);
+	free(result);
+	return 0;
+}
