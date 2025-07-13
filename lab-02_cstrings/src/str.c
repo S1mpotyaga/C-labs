aae92343f0496702c89e0ffd499c761595acd9a6
S1mpotyaga <zfk.2024@gmail.com>
2025-07-13 18:50:33 +0500

    add brackets

--- lab-02_cstrings/src/str.c
+++ lab-02_cstrings/src/str.c
@@ -4,15 +4,17 @@
 char *my_strcpy(char *dest, const char *src){
 	char *it_dest = (char *)dest;
 	const char *it_src = src;
-	for (; *it_src != '\0'; it_src++, it_dest++)
+	for (; *it_src != '\0'; it_src++, it_dest++){
 		*it_dest = *it_src;
+	}
 	return dest;
 }
 
 char *my_strcat(char *dest, const char *src){
 	char *it_dest = dest;
-	while (*it_dest != '\0')
+	while (*it_dest != '\0'){
 		it_dest++;
+	}
 	it_dest = my_strcpy(it_dest, src);
 	return dest;
 }
@@ -20,9 +22,11 @@ char *my_strcat(char *dest, const char *src){
 int my_strcmp(const char *lhs, const char *rhs){
 	const char *it_lhs = lhs;
 	const char *it_rhs = rhs;
-	for (; *it_lhs != '\0' && *it_rhs != '\0'; it_lhs++, it_rhs++)
-		if (*it_lhs != *it_rhs)
+	for (; *it_lhs != '\0' && *it_rhs != '\0'; it_lhs++, it_rhs++){
+		if (*it_lhs != *it_rhs){
 			break;
+		}
+	}
 	return *it_lhs - *it_rhs;
 }
                                                              
