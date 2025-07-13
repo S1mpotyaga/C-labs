dbcf65f7b8dc21f89a57a9d1729f36c91439ddf7
S1mpotyaga <zfk.2024@gmail.com>
2025-07-13 18:15:40 +0500

    all done

--- lab-02_cstrings/src/test_str.c
+++ lab-02_cstrings/src/test_str.c
@@ -6,50 +6,57 @@
 #include <stdbool.h>
 
 void test_strcpy(){
-	char a[] = "Hello";
-	char b[] = "aba";
-	char c[] = "";
-	char a_cpy[10] = "";
-	char b_cpy[10] = "acd";
-	char c_cpy[10] = "a";
-	assert(strcmp(my_strcpy(a_cpy, a), a) == 0);
-	assert(strcmp(my_strcpy(b_cpy, b), b) == 0);
-	assert(strcmp(my_strcpy(c_cpy, c), c) == 0);
+	char Hello[] = "Hello";
+	char empty_string_1[10] = "";
+	assert(strcmp(my_strcpy(empty_string_1, Hello), Hello) == 0);
+
+	char aba[] = "aba";
+	char acd[10] = "acd";
+	assert(strcmp(my_strcpy(acd, aba), aba) == 0);
+
+	char empty_string_2[] = "";
+	char test_string[10] = "a";
+	assert(strcmp(my_strcpy(test_string, empty_string_2), empty_string_2) == 0);
 }
 
 void test_strcat(){
-	char a[] = "Hello";
-	char b[] = "";
-	char c[] = "me";
-	char d[] = "";
-	char a_tmp[] = "!";
-	char b_tmp[] = "Hello!";
-	char c_tmp[] = ":)";
-	char d_tmp[] = "";
-	char check1[] = "Hello!";
-	char check2[] = "me:)";
-	char check3[] = "";
-	assert(strcmp(my_strcat(a, a_tmp), check1) == 0);
-	assert(strcmp(my_strcat(b, b_tmp), check1) == 0);
-	assert(strcmp(my_strcat(c, c_tmp), check2) == 0);
-	assert(strcmp(my_strcat(d, d_tmp), check3) == 0);
+	char Hello_1[] = "Hello";
+	char exclamation_mark[] = "!";
+	char result_string_1[] = "Hello!";
+	assert(strcmp(my_strcat(Hello_1, exclamation_mark), result_string_1) == 0);
+	
+	char empty_string_1[] = "";
+	char Hello_2[] = "Hello!";
+	assert(strcmp(my_strcat(empty_string_1, Hello_2), result_string_1) == 0);
+	
+	char me[] = "me";
+	char smile[] = ":)";
+	char result_string_2[] = "me:)";
+	assert(strcmp(my_strcat(me, smile), result_string_2) == 0);
+
+	char empty_string_2[] = "";
+	char empty_string_3[] = "";
+	char result_string_3[] = "";
+	assert(strcmp(my_strcat(empty_string_2, empty_string_3), result_string_3) == 0);
 }
 
 
 void test_strcmp(){
-	char a[] = "aba";
-	char b[] = "ajfgkerngjea";
-	char c[] = "";
-	assert(my_strcmp(a, b) == strcmp(a, b));
-	assert(my_strcmp(a, c) == strcmp(a, c));
-	assert(my_strcmp(b, c) == strcmp(b, c));
+	char test_string_1[] = "aba";
+	char test_string_2[] = "ajfgkerngjea";
+	char empty_string[] = "";
+	assert(my_strcmp(test_string_1, test_string_2) == strcmp(test_string_1, test_string_2));
+	assert(my_strcmp(test_string_1, empty_string) == strcmp(test_string_1, empty_string));
+	assert(my_strcmp(test_string_2, empty_string) == strcmp(test_string_2, empty_string));
 }
 
 void test_strlen(){
-	char a[] = "ajbsdfjkne";
-	char b[] = "";
-	char c[] = "\n";
-	assert(my_strlen(a) == strlen(a));
-	assert(my_strlen(b) == strlen(b));
-	assert(my_strlen(c) == strlen(c));
+	char test_string[] = "ajbsdfjkne";
+	assert(my_strlen(test_string) == strlen(test_string));
+
+	char empty_string[] = "";
+	assert(my_strlen(empty_string) == strlen(empty_string));
+
+	char new_line[] = "\n";
+	assert(my_strlen(new_line) == strlen(new_line));
 }
